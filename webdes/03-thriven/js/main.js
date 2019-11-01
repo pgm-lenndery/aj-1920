df202573f208506858c67456e3084990ff9f13e4
Lennert De Ryck <32176355+pgm-lenndery@users.noreply.github.com>
2019-11-01 15:49:00 +0100

    Added project

---
+++ webdes/03-thriven/js/main.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('#btnCollapse');
+const nav = document.querySelector('header');
+
+btn.addEventListener('click', () => {
+    console.log('cliked');
+    nav.classList.toggle('show');
+    nav.classList.toggle('hidden');
+})
+
+document.addEventListener("click", function(event) {
+	// If user clicks inside the element, do nothing
+	if (event.target.closest("nav") || event.target.closest('#btnCollapse')) return;
+
+    // If user clicks outside the element, hide it!
+    nav.classList.remove('show');
+    nav.classList.add('hidden');
+});
