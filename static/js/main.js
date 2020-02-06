f17147adfdaab427234c03c6fc084a79477b247f
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-02-06 17:05:24 +0100

    Update

--- static/js/main.js
+++ static/js/main.js
@@ -63,6 +63,11 @@
                     descr: 'Het namaken van https://matthiastalloen.be/',
                     href: '/at-work/02-case-2'
                 },
+                {
+                    title: 'Case 3 Matthias Talloen',
+                    descr: 'Het namaken van https://matthiastalloen.be/',
+                    href: '/at-work/02-case-3'
+                },
             ]
         }
 
